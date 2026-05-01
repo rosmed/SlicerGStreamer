9d34332ce8d940f9e7433ba2dbdcd4c54a9b4da4
Anton Deguet <anton.deguet@jhu.edu>
2026-05-01 17:07:27 -0400

    Added some examples

--- qSlicerGStreamerModule.h
+++ qSlicerGStreamerModule.h
@@ -21,7 +21,19 @@ public:
 
   qSlicerGetTitleMacro("GStreamer");
 
-  QString helpText() const override { return "This module allows streaming images to/from GStreamer."; }
+  QString helpText() const override {
+    return "This module allows streaming images to/from GStreamer.<br><br>"
+      "<b>Stream Out Example:</b><br>"
+      "In Slicer, create a Streamer node with Stream Out. Then run:<br>"
+      "gst-launch-1.0 unixfdsrc socket-path=/tmp/slicer_gstreamer_View1_anton.sock ! videoconvert ! autovideosink<br><br>"
+      "<b>Stream In Example:</b><br>"
+      "In Slicer, create a Streamer node with Stream In and socket path /tmp/videotest. Then run:<br>"
+      "gst-launch-1.0 videotestsrc ! videoconvert ! unixfdsink socket-path=/tmp/videotest<br><br>"
+      "<b>WebRTC Note:</b><br>"
+      "WebRTC streaming uses webrtcbin and requires a signaling server. It does not use a simple IP/Port like RTSP.<br>"
+      "You can test the pipeline locally using:<br>"
+      "gst-launch-1.0 webrtcbin name=ws ! rtph264pay ! fakesink (requires signaling integration)";
+  }
   QString acknowledgementText() const override { return "Developed by Anton Deguet."; }
   QStringList contributors() const override { return QStringList() << "Anton Deguet"; }
 
